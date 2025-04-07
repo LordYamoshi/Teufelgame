9c2c96a6185115935f18109e1de50fca2c4d3f5e
Mikeysaur <mikemadan06@gmail.com>
2025-04-07 23:35:17 +0200

    Dit is de laatste commit.. hopelijk
    Camera nieuwe positie
    handmatige resetknop (f5)

--- Assets/Scripts/MainGameSystems/GameStateManager.cs
+++ Assets/Scripts/MainGameSystems/GameStateManager.cs
@@ -811,4 +811,12 @@ public class GameStateManager : MonoBehaviour
         UnityEngine.SceneManagement.SceneManager.LoadScene(
             UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F5))
+        {
+            RestartGame();
+        }
+    }
 }
